1abd95847c77fcb94da90d01c81bc1b7902cdebd
yunyiseul <8953yls@hanmail.net>
2015-09-01 18:42:43 +0900

    navigation 수정

--- gulpfile.js
+++ gulpfile.js
@@ -86,10 +86,10 @@ gulp.task('jade', function() {
 // 변경 업무: (sass|scss) → CSS
 gulp.task('sass', function() {
 	// gulp-ruby-sass 사용 시에는 디렉토리 명만 입력할 것!
-	return rubySass('src/sass', {'style': 'compact'})
+	return rubySass('src/sass',config.ruby_sass)
 		.on('error', rubySass.logError)
 		.pipe( sourcemaps.write(config.ruby_sass_sourcemaps.dir, config.ruby_sass_sourcemaps.options) )
-		.pipe(mq())
+		// .pipe(mq())
 		.pipe( gulp.dest('dist/css') )
 		.pipe( filter('**/*.css') )
 		.pipe( reload({stream: true}) );
